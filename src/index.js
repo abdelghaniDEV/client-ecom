a599294dcb83fbff5e3fe942492b07c1686f07a4
abdelghaniDEV <abdelghaniberhouch2001@gmail.com>
2024-10-17 14:51:49 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
   <RecoilRoot>
-  <BrowserRouter basename='/ecommerce-demo'>
+  <BrowserRouter basename='/safwa-ecommerce'>
    <Provider store={store}>
     <App />
     </Provider>
